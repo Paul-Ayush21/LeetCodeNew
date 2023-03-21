b71905c894a008c234f3584e825856183eb5b185
notNakul <80635339+notNakul@users.noreply.github.com>
2023-03-21 20:10:15 +0530

    Time: 195 ms (60.20%), Space: 29.8 MB (43.70%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        
         vector<vector<int>> res;
-        
-        for(int i=0;i<nums.size()-2;i++){
-            if(i==0||(i>0 && nums[i]!=nums[i-1])){
-                
-                int low=i+1;
-                int high = nums.size()-1;
-                // int sum = 0-
-                while(low<high){
-                    if(nums[low]+nums[high]==(-nums[i])){
-                        vector<int> temp;
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[low]);
-                        temp.push_back(nums[high]);
-                        res.push_back(temp);
-                        
-                        while(low<high && nums[low]==nums[low+1]) low++;
-                        while(low<high && nums[high]==nums[high-1]) high--;
-                        low++;
-                        high--;
-                    }
-                    else if(nums[low]+nums[high]>(-nums[i])){
-                        high--;
-                    }
-                    else{
-                        low++;
-                    }
-                    
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<(int)(nums.size())-2;i++){
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+                int left=i+1,high=(int)(nums.size())-1;
+                int sum=0-(nums[i]);
+                while(left<high){
+                 if(nums[left]+nums[high]==sum){
+                    vector<int> temp;
+                    temp.push_back(nums[left]);
+                    temp.push_back(nums[high]);
+                    temp.push_back(nums[i]);
+                    res.push_back(temp);
+                     
+                     while(left<high && nums[left]==nums[left+1]) left++;
+                     while(left<high && nums[high]==nums[high-1]) high--;
+                     left++;
+                     high--;
+                 }
+                 else if(nums[left]+nums[high]>sum) high--;
+                 else left++;
                 }
             }
         }

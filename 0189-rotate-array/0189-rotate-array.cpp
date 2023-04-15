571c20636688fb8b7f1d044fb932d524b05b2311
notNakul <80635339+notNakul@users.noreply.github.com>
2023-04-15 10:20:04 +0530

    Time: 41 ms (9.28%), Space: 24.9 MB (94.94%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int g=k%nums.size();
-        vector<int> res;
-        for(int i=nums.size()-g;i<nums.size();i++){
-            res.push_back(nums[i]);
-        }
-        for(int i=0;i<nums.size()-g;i++){
-            res.push_back(nums[i]);
-        }
-        nums.clear();
-        copy(res.begin(),res.end(),back_inserter(nums));
+        // int g=k%nums.size();
+        // vector<int> res;
+        // for(int i=nums.size()-g;i<nums.size();i++){
+        //     res.push_back(nums[i]);
+        // }
+        // for(int i=0;i<nums.size()-g;i++){
+        //     res.push_back(nums[i]);
+        // }
+        // nums.clear();
+        // copy(res.begin(),res.end(),back_inserter(nums));
+        k=k%nums.size();
+        reverse(nums.begin(),nums.begin()+nums.size()-k);
+        reverse(nums.begin()+nums.size()-k,nums.begin()+nums.size());
+        reverse(nums.begin(),nums.begin()+nums.size());
     }
 };

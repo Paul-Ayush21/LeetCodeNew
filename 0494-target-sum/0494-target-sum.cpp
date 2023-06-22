0a6366aad8a0f9fb4b5d532c1236f28e6f96ccf3
notNakul <80635339+notNakul@users.noreply.github.com>
2023-06-22 21:14:24 +0530

    Time: 29 ms (51.79%), Space: 49.3 MB (15.05%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int f(int ind,vector<int>& nums,int target){
+    int f(int ind,vector<int>& nums,int target,vector<vector<int>>& dp){
         if(ind==nums.size()){
             // cout<<target<<endl;
             if(target==0){
@@ -10,12 +10,19 @@ public:
             // cout<<"HERE"<<endl;
             return 0;
         }
-        
-        int plus=+nums[ind]+f(ind+1,nums,target+nums[ind]);
-        int minus=-nums[ind]+f(ind+1,nums,target-nums[ind]);
-        return plus+minus;
+        if(dp[ind][target+1000]!=-1){return dp[ind][target+1000];}
+        int plus=+nums[ind]+f(ind+1,nums,target+nums[ind],dp);
+        int minus=-nums[ind]+f(ind+1,nums,target-nums[ind],dp);
+        return dp[ind][target+1000]=plus+minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return f(0,nums,target);
+        int sum=0;
+        for(auto it:nums){
+            sum+=it;
+        }
+        // cout<<sum;
+        vector<vector<int>> dp(nums.size()+1,vector<int>(sum*2+1+2005,-1));
+        return f(0,nums,target,dp);
+        // return 0;
     }
 };

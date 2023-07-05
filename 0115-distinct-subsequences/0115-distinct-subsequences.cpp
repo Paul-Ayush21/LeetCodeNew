b77f0e47a455da7fa5e6a4ccac5257442d07a99a
notNakul <80635339+notNakul@users.noreply.github.com>
2023-07-05 14:38:39 +0530

    Time: 56 ms (48.03%), Space: 30.2 MB (27.13%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int f(int i,int j,string& s,string& t,vector<vector<int>>& dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
+        if(j==0) return 1;
+        if(i==0) return 0;
         
         
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]){
+        if(s[i-1]==t[j-1]){
             return dp[i][j]= (f(i-1,j-1,s,t,dp) + f(i-1,j,s,t,dp));
         }
         else{
@@ -14,8 +14,35 @@ public:
         }
     }
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.size(),vector<int>(t.size(),-1));
-        int k=f(s.size()-1,t.size()-1,s,t,dp);
-        return k;
+        vector<vector<double>> dp(s.size()+1,vector<double>(t.size()+1,0));
+        // int k=f(s.size(),t.size(),s,t,dp);
+        // return k;
+        for(int i=0;i<=s.size();i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=t.size();i++){
+            dp[0][i]=0;
+        }
+        
+        for(int i=1;i<=s.size();i++){
+            for(int j=1;j<=t.size();j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=(dp[i-1][j-1]+dp[i-1][j]);
+                    // cout<<dp[i][j]<<endl;
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                    // cout<<dp[i][j]<<endl;
+                }
+            }
+        }
+        // for(int i=0;i<=s.size();i++){
+        //     for(int j=0;j<=t.size();j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[s.size()][t.size()];
     }
+    
 };

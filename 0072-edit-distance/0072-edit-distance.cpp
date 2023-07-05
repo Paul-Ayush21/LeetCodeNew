894f3640aa32e3b8bdc81bad9a38c4918bc625dd
notNakul <80635339+notNakul@users.noreply.github.com>
2023-07-05 17:24:03 +0530

    Time: 28 ms (26.75%), Space: 8.9 MB (74.54%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -11,7 +11,24 @@ public:
         return dp[i][j]= 1+min(min(f(i,j-1,s,t,dp),f(i-1,j,s,t,dp)),f(i-1,j-1,s,t,dp));
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size(),vector<int>(word2.size(),-1));
-        return f(word1.size()-1,word2.size()-1,word1,word2,dp);
+        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,0));
+        // return f(word1.size()-1,word2.size()-1,word1,word2,dp);
+        
+        for(int i=0;i<=word1.size();i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=word2.size();j++){
+            dp[0][j]=j;
+        }
+        
+        for(int i=1;i<=word1.size();i++){
+            for(int j=1;j<=word2.size();j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else dp[i][j]= 1+min(min(dp[i][j-1],dp[i-1][j]),dp[i-1][j-1]);
+            }
+        }
+        return dp[word1.size()][word2.size()];
     }
 };

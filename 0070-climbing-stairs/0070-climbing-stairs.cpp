7c4e2bc37463fa781e2c0697ab3036d1ac6fc271
notNakul <80635339+notNakul@users.noreply.github.com>
2023-08-13 22:33:25 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (14.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,43 +1,22 @@
 class Solution {
 public:
-    int f(int ind,int n,vector<long long>& dp){
-        if(ind==0) return 1;
-        
-        int m1,m2;
-        if(ind-1>=0){
-            m1=f(ind-1,n,dp);
-        }
-        if(ind-2>=0){
-            m2=f(ind-2,n,dp);
-        }
-        return dp[ind]=m1+m2;
-        
+    int f(int n,vector<long long>& dp){
+        if(n==0 || n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=f(n-1,dp) + f(n-2,dp);
     }
     int climbStairs(int n) {
         vector<long long> dp(n+1,-1);
-        // return f(n,n,dp);
+        return f(n,dp);
         
-        dp[0]=1;
-        dp[1]=1;
+//         dp[0]=1;
+//         dp[1]=1;
         
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
+//         for(int i=2;i<=n;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
         
-        return dp[n];
-        dp[n] = 1;
-        dp[n-1]=1;
+//         return dp[n];
         
-        for(int i=n-2;i>0;i--){
-                int m1,m2;
-                if(i+1<n-1){
-                    m1=dp[i+1];
-                }
-                if(i+2<n-1){
-                    m2=dp[i+2];
-                }
-                dp[i]=m1+m2;
-        }
-        return dp[1];
     }
 };
